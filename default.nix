3ef6e66c7e931b3e7ae44175c7e2eec747a06069
jcleng <j.c.leng@foxmail.com>
2021-02-14 00:39:25 +0800

    提交
    Signed-off-by: jcleng <j.c.leng@foxmail.com>

--- default.nix
+++ default.nix
@@ -1,23 +1,16 @@
+with import <nixpkgs> {}; stdenv.mkDerivation {
+  name = "dev-jcleng-php72-darwin-7.2.33";
 
-{ pkgs ? import <nixpkgs> { } }:
-with pkgs;
+  # Source Code
+  # See: https://nixos.org/nixpkgs/manual/#ssec-unpack-phase
+  src = ./src;
 
-stdenv.mkDerivation rec {
-  pname = "dev-jcleng-php72-darwin";
-  version = "7.2.33";
-
-  src = fetchurl {
-     url = "https://github.91chifun.workers.dev//https://github.com/php/php-src/archive/php-7.2.33.tar.gz";
-     sha256 = "7354574c7b3b1e3f56831223c7889fc8ccd2d30422a294b3bf68a450255e88ba";
-  };
-  # 手动下载
-  # wget https://github.91chifun.workers.dev//https://github.com/php/php-src/archive/php-7.2.33.tar.gz
-  # src = ./src;
-  # libXpm :https://github.com/NixOS/nixpkgs/commit/f34685600ec397c742a7f5048d57475d0f0b692e
-  # libaio
+  # Dependencies
+  # See: https://nixos.org/nixpkgs/manual/#ssec-stdenv-dependencies
   buildInputs = [ tree coreutils pkg-config re2c libwebp libzip xorg.libXpm bison zlib git libxml2 openssl autoconf automake libtool gcc10  pcre curl libpng libjpeg freetype gd libiconv];
-  # doCheck = true;
-  # tree -L 3 ${libiconv}
+
+  # Build Phases
+  # See: https://nixos.org/nixpkgs/manual/#sec-stdenv-phases
   configurePhase = ''
     echo "初始化:"
     ./buildconf --force
@@ -65,18 +58,12 @@ stdenv.mkDerivation rec {
 --with-webp-dir=${libwebp} \
 --with-xpm-dir=${xorg.libXpm.dev}
   '';
+  buildPhase = ''
+    echo "编译:"
+    make -j4
+  '';
   installPhase = ''
-    echo "编译&&安装:"
-    make
+    echo "安装:"
     make install
   '';
-  meta = with lib; {
-    description = "php72";
-    longDescription = ''
-      增加一个dev-php环境的包
-    '';
-    homepage = "";
-    changelog = "";
-    platforms = platforms.all;
-  };
 }
