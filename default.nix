819970c28d16258db069b8e5e88fbb3cc84dbb0d
jcleng <j.c.leng@foxmail.com>
2021-07-13 22:10:32 +0800

    linux use mysqli fix --with-iconv-dir use src

--- default.nix
+++ default.nix
@@ -1,14 +1,14 @@
 with import <nixpkgs> {}; stdenv.mkDerivation {
-  name = "dev-jcleng-php71-darwin-7.1.13";
+  name = "dev-jcleng-php71-linux-7.1.13";
 
   # Source Code
   # See: https://nixos.org/nixpkgs/manual/#ssec-unpack-phase
   # wget https://github.91chifun.workers.dev//https://github.com/php/php-src/archive/php-7.1.13.tar.gz
-  # src = ./src;
-  src = fetchurl {
-     url = "https://github.com/php/php-src/archive/php-7.1.13.tar.gz";
-     sha256 = "eaab3bed90f28e6232ee9bf5510b6533e5c1c8777542ad45db75a027cd2dbb85";
-  };
+  src = ./src;
+  # src = fetchurl {
+  #    url = "https://github.com/php/php-src/archive/php-7.1.13.tar.gz";
+  #    sha256 = "eaab3bed90f28e6232ee9bf5510b6533e5c1c8777542ad45db75a027cd2dbb85";
+  # };
   # Dependencies
   # See: https://nixos.org/nixpkgs/manual/#ssec-stdenv-dependencies
   buildInputs = [ tree coreutils pkg-config re2c libwebp libzip xorg.libXpm bison zlib git libxml2 openssl autoconf automake libtool gcc10  pcre curl libpng libjpeg freetype gd libiconv];
@@ -28,10 +28,11 @@ with import <nixpkgs> {}; stdenv.mkDerivation {
 --mandir=$out/php/man \
 --with-config-file-path=$out/etc \
 --with-pdo-mysql \
+--with-mysqli \
 --with-mhash \
 --with-openssl \
 --with-gd=${gd.dev}/include \
---with-iconv=${libiconv}/ \
+--with-iconv-dir=${libiconv}/ \
 --with-zlib=${zlib.dev} \
 --with-libxml-dir=${libxml2.dev} \
 --with-libzip=${libzip.dev} \
